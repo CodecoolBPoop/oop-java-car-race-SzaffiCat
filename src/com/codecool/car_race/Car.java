5f3efab306c6aa2c5898767003c410937900e9ad
Krisztina Nagy <nakuaat@gmail.com>
2018-12-07 18:03:56 +0100

    created classes

---
+++ src/com/codecool/car_race/Car.java
@@ -0,0 +1,16 @@
+package com.codecool.car_race;
+
+public class Car {
+
+    private int normalSpeed;
+    private String name;
+    private int distanceTraveled;
+
+    private int prepareForLap(Race race) {
+        return normalSpeed;
+    }
+
+    public void moveForAnHour() {
+
+    }
+}
---
+++ src/com/codecool/car_race/Weather.java
@@ -0,0 +1,13 @@
+package com.codecool.car_race;
+
+public class Weather {
+
+    public void setRaining() {
+        //TODO 30% chance of rain
+    }
+
+    public boolean isRaining() {
+        //Todo return that it is raining or not
+        return false;
+    }
+}
